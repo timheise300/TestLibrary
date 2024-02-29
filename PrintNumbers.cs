04a0d90ffb32c0ffebbc617a538371ecdc12393e
Tim <timheise300@gmail.com>
2024-02-29 14:35:37 -0500

    Updated readme

--- PrintNumbers.cs
+++ PrintNumbers.cs
@@ -1,44 +1,43 @@
-﻿namespace TestLibray
+﻿namespace TestLibrary;
+
+public static class PrintNumbers
 {
-    public static class PrintNumbers
+    /// <summary>
+    /// Takes in a list of string pairs and a list of int pairs and returns a combined list based on 
+    /// all string pairs replacing all divisible members with their related string pairs.
+    /// </summary>
+    /// <param name="count">Total amount of strings to return</param>
+    /// <param name="intPairs">Integer pair for checking divisibility</param>
+    /// <param name="stringPairs">Dictionary of string pairs for replacing with one or both strings depending on divisibility</param>
+    /// <returns>List of integer strings with specified divisible members replaced by the specified string or string pairs</returns>
+    public static List<string> GetRangeStrings(int count, KeyValuePair<int, int> intPair, Dictionary<string, string> stringPairs)
     {
-        /// <summary>
-        /// Takes in a list of string pairs and a list of int pairs and returns a combined list based on 
-        /// all string pairs replacing all divisible members with their related string pairs.
-        /// </summary>
-        /// <param name="count">Total amount of strings to return</param>
-        /// <param name="intPairs">Dictionary of integer pairs for checking divisibility</param>
-        /// <param name="stringPairs">Dictionary of string pairs for replacing with one or both strings depending on divisibility</param>
-        /// <returns>List of integer strings with specified divisible members replaced by the specified string or string pairs</returns>
-        public static List<string> GetRangeStrings(int count, KeyValuePair<int, int> intPair, Dictionary<string, string> stringPairs)
+        List<string> rangeStrings = [];
+
+        foreach (KeyValuePair<string, string> stringPair in stringPairs)
         {
-            List<string> rangeStrings = [];
+            GetRangeStringsRecursive(count, intPair, stringPair, rangeStrings, 0);
+        }
 
-            foreach (KeyValuePair<string, string> stringPair in stringPairs)
-            {
-                GetRangeStringsRecursive(count, intPair, stringPair, rangeStrings, 0);
-            }
+        return rangeStrings;
+    }
 
+    //Recursive function to avoid nested loops
+    private static List<string> GetRangeStringsRecursive(int count, KeyValuePair<int, int> intPair, KeyValuePair<string, string> stringPair, List<string> rangeStrings, int i)
+    {
+        if (i >= count)
+        {
             return rangeStrings;
         }
 
-        //Recursive function to avoid nested loops
-        private static List<string> GetRangeStringsRecursive(int count, KeyValuePair<int, int> intPair, KeyValuePair<string, string> stringPair, List<string> rangeStrings, int i)
+        rangeStrings.Add(i switch
         {
-            if (i >= count)
-            {
-                return rangeStrings;
-            }
+            var x when (x % intPair.Key == 0 && x % intPair.Value == 0) => $"{stringPair.Key} {stringPair.Value}",
+            var x when (x % intPair.Key == 0) => stringPair.Value,
+            var x when (x % intPair.Value == 0) => stringPair.Key,
+            _ => i.ToString()
+        });
 
-            rangeStrings.Add(i switch
-            {
-                var x when (x % intPair.Key == 0 && x % intPair.Value == 0) => $"{stringPair.Key} {stringPair.Value}",
-                var x when (x % intPair.Key == 0) => stringPair.Value,
-                var x when (x % intPair.Value == 0) => stringPair.Key,
-                _ => i.ToString()
-            });
-
-            return GetRangeStringsRecursive(count, intPair, stringPair, rangeStrings, i + 1);
-        }
+        return GetRangeStringsRecursive(count, intPair, stringPair, rangeStrings, i + 1);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# TestLibray
+# TestLibrary
+
+**Possible Updates**
+- Consider passing a class containing the count, dictionary, and integer pair to reduce the amount of parameters, and avoid having to update the method signature should future updates need more information passed.
+- Adding error handling
+- Performance optimization may be necessary depending on the size of the "count" argument
